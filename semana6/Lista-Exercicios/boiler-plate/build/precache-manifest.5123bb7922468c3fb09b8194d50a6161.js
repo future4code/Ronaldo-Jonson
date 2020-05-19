6137a006a757d9ccb8aa0d8072cede128b1e376a
Ronaldo Jonson <ronaldojonson@hotmail.com>
2020-05-19 16:01:34 -0300

    Entrega-Exercicios

---
+++ semana6/Lista-Exercicios/boiler-plate/build/precache-manifest.5123bb7922468c3fb09b8194d50a6161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80bae9e32dacde772e7e5207fa1fbf2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b0965c834fe94b5c186",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "28e1f6e4c00be6f90c95",
+    "url": "/static/js/2.8fdc7c9c.chunk.js"
+  },
+  {
+    "revision": "1b0965c834fe94b5c186",
+    "url": "/static/js/main.f29cecc7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
