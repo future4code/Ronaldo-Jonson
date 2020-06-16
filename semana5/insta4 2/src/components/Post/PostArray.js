972f93161729ce433a2730ca0edc87a36ad0bb11
Ronaldo Jonson <ronaldojonson@hotmail.com>
2020-06-16 12:07:56 -0300

    entrega-semana-9

--- semana5/insta4 2/src/components/Post/PostArray.js
+++ semana5/insta4 2/src/components/Post/PostArray.js
@@ -2,28 +2,7 @@ import React from 'react'
 import Post from './Post';
 
 class PostArray extends React.Component {
-	state = {
-		Posts: [
-            {
-                nomeUsuario: 'Paulinha',
-                fotoUsuario: 'https://picsum.photos/50/50',
-                fotoPost: 'https://picsum.photos/200/200'
-            },
-            {
-                nomeUsuario: 'Caio',
-                fotoUsuario: 'https://picsum.photos/40/40',
-                fotoPost: 'https://picsum.photos/100/100'
-            },
-            {
-                nomeUsuario: 'Danvas',
-                fotoUsuario: 'https://picsum.photos/60/60',
-                fotoPost: 'https://picsum.photos/100/150'
-            }
-        ]
-    }
-
 	render() {   
-
         const timeline = this.state.Posts.map(element => {
             return <Post
                 key = {element.nomeUsuario}
